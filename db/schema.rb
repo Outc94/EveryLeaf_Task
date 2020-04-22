a8cdeebd4c54833914b82e9343a532dcd3c229e7
Mouhamed Diop <outc94@hotmail.fr>
2020-04-23 00:15:41 +0900

    Added Column Status to Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_151019) do
+ActiveRecord::Schema.define(version: 2020_04_22_151417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_04_22_151019) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "start_at"
     t.datetime "end_at"
+    t.text "status"
   end
 
 end
