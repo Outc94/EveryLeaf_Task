2fc2f26dfeed2270d235b1c6195f14401d124101
Mouhamed Diop <outc94@hotmail.fr>
2020-04-22 12:23:29 +0900

    Applied Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_074814) do
+ActiveRecord::Schema.define(version: 2020_04_22_031126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.text "title"
-    t.text "content"
+    t.string "title", null: false
+    t.string "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
