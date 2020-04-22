8e196f68ff33d7a4b05cd9e45ac7db56999b96bc
Mouhamed Diop <outc94@hotmail.fr>
2020-04-22 12:35:50 +0900

    Test for validation on the model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "Validation does not pass if the title is empty" do
+    task = Task.new(title: '', content: 'This Test should Fail')
+    expect(task).not_to be_valid
+  end
+
+  it "Validation does not pass if content is empty" do
+    task = Task.new(tittle: 'This one as well', content: '')
+    expect(task).not_to be_valid
+  end
+
+  it "validation passes If content is described in title and content" do
+    task = Task.new(tittle: 'This one passes', content: 'The good one')
+    expect(task).not_to be_valid
+  end
 end
