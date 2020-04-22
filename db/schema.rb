2eafd2bc024d5c9003b70b7a31d348443b0d212f
Mouhamed Diop <outc94@hotmail.fr>
2020-04-23 00:20:57 +0900

    Added Column Priority to Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_151417) do
+ActiveRecord::Schema.define(version: 2020_04_22_151856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_04_22_151417) do
     t.datetime "start_at"
     t.datetime "end_at"
     t.text "status"
+    t.index ["status"], name: "index_tasks_on_status"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
