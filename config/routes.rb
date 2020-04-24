fad24ee3987284cb7174b388126005ae8a4c2f5f
Mouhamed Diop <outc94@hotmail.fr>
2020-04-24 23:16:29 +0900

    Prepared the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources:tasks
-  root "tasks#index"
+  root 'sessions#new'
+  resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only:[:new, :create, :show]
+
+  namespace :admin do
+    resources :users, only:[:index, :edit, :update, :new, :create, :show, :destroy]
+  end
 end
