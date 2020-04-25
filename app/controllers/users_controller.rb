631e2bcfae04d2c27c52bd3ae23b6618d549d462
Mouhamed Diop <outc94@hotmail.fr>
2020-04-25 23:18:49 +0900

    Added function to avoid going to users registration page or another user's show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :only_see_own_page, only: [:show]
+  before_action :already_in, only:[:new, :create]
+
   def new
     @user = User.new
   end
@@ -24,5 +27,18 @@ class UsersController < ApplicationController
     params.require(:user).permit(:firstname, :lastname, :email, :password,
                                  :password_confirmation)
   end
-  
+
+  def only_see_own_page
+    @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to user_path(current_user.id), notice: "You are only allowed to view your own profile page."
+    end
+  end
+
+  def already_in
+   if current_user
+     redirect_to tasks_path,  notice: "You can't create user when signed in unless you are an Admin (go to User Management if so)"
+   end
+ end
+
 end
