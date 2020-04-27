0fbcfb4e36058061468891f2ae98b15ee45c0f46
Mouhamed Diop <outc94@hotmail.fr>
2020-04-27 16:49:35 +0900

    Created and Pass the tests for Labels list, creation and attached to tasks

---
+++ spec/features/label.spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+RSpec.feature "Label Management Function", type: :feature do
+  background do
+    FactoryBot.create(:user, firstname: 'Mouhamed',
+                             lastname: 'Diop',
+                             email: 'outc@mail.com',
+                             password: 'testing1234',
+                             password_confirmation: 'testing1234')
+    visit new_session_path
+    fill_in 'email', with: 'outc@mail.com'
+    fill_in 'Password', with: 'testing1234'
+    click_button 'Log in'
+    @user = User.first
+    FactoryBot.create(:label, name: "label1", user_id: @user.id)
+    FactoryBot.create(:label, name: "label2", user_id: @user.id)
+    FactoryBot.create(:label, name: "label3", user_id: @user.id)
+    FactoryBot.create(:task, title: 'Testing',
+                             status: 'Not started',
+                             content: 'testing the content',
+                             priority:'low',
+                             start_at: '2022-10-23',
+                             end_at: '2029-11-12',
+                             user_id: @user.id)
+  end
+  scenario "Test Label list" do
+    visit labels_path
+    save_and_open_page
+    expect(page).to have_content 'label1'
+  end
+
+  scenario "Test Label Creation" do
+    FactoryBot.create(:user, firstname: 'Moustapha',
+                             lastname: 'Dia',
+                             email: 'out@mail.com',
+                             password: 'testing1234',
+                             password_confirmation: 'testing1234')
+    visit new_session_path
+    fill_in 'email', with: 'out@mail.com'
+    fill_in 'Password', with: 'testing1234'
+    click_button 'Log in'
+    visit new_label_path
+    fill_in 'Name', with: 'label4'
+    click_button 'Save'
+    expect(page).to have_content 'label4'
+  end
+
+  scenario "Test Label attached to task" do
+    @task = Task.first
+    @label1 = Label.first
+    @label2 = Label.second
+    @task.labels = [@label1, @label2]
+    @task.save
+    visit tasks_path
+    fill_in 'search_label', with: 'label1'
+    expect(page).to have_content 'label1'
+  end
+end
